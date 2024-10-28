fcff6bdef252df3f6a7d8c84213fbaafe7b79ba4
Ventax <maulanakenang24@gmail.com>
2024-10-28 14:41:42 +0700

    LARAVEL-CRUD

---
+++ resources/views/instalasi.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Instalasi Blade </h1>
+    <ul>
+        <li>composer create-project laravel/laravel example-app</li>
+        <li>cd-example app</li>
+        <li>php artisan serve</li>
+        <li>buka browser akses, akses url localhost:8000</li>
+    </ul>
+</body>
+</html>
